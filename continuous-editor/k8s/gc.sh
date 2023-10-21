fd272c84aa86699d7be5482321c6da3ef9b2dd2f
Ana <mihalceanuana@gmail.com>
2023-10-21 21:55:29 +0200

    Add Xms and Xmx for G1GC throughput improvement

--- continuous-editor/k8s/gc.sh
+++ continuous-editor/k8s/gc.sh
@@ -1,5 +1,5 @@
 ## g1gc tunning
-kubectl set env deployment/continuous-editor JDK_JAVA_OPTIONS="-XX:MaxGCPauseMillis=250 -XX:ParallelGCThreads=4 -XX:ConcGCThreads=1 -Xlog:gc*=debug:file=/tmp/gc.log"
+kubectl set env deployment/continuous-editor JDK_JAVA_OPTIONS="-XX:MaxGCPauseMillis=250 -Xmx2816m -Xmx2816m -XX:ParallelGCThreads=4 -XX:ConcGCThreads=1 -Xlog:gc*=debug:file=/tmp/gc.log"
 
 ## parallel gc
 kubectl set env deployment/continuous-editor JDK_JAVA_OPTIONS="-XX:+UseParallelGC -Xlog:gc*=debug:file=/tmp/gc.log"
--- eager-factorial/k8s/gc.sh
+++ eager-factorial/k8s/gc.sh
@@ -1,5 +1,5 @@
 ## g1gc tunning
-kubectl set env deployment/eager-factorial JDK_JAVA_OPTIONS="-XX:MaxGCPauseMillis=150 -Xlog:gc*=debug:file=/tmp/gc.log"
+kubectl set env deployment/eager-factorial JDK_JAVA_OPTIONS="-XX:MaxGCPauseMillis=150 -Xms1024m -Xmx1024m -Xlog:gc*=debug:file=/tmp/gc.log"
 
 ## parallel gc
 kubectl set env deployment/eager-factorial JDK_JAVA_OPTIONS="-XX:+UseParallelGC -Xlog:gc*=debug:file=/tmp/gc.log"
