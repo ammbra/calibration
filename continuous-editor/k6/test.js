ec98cbb1ff250a8d8a8503e7183f5aeae2abee09
Ana <mihalceanuana@gmail.com>
2023-10-27 17:09:13 +0200

    Update comments in k6 tests

--- continuous-editor/k6/test.js
+++ continuous-editor/k6/test.js
@@ -5,8 +5,8 @@ import { URLSearchParams } from 'https://jslib.k6.io/url/1.0.0/index.js';
 export const options = {
     summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)', 'p(99.99)', 'count'],
     stages: [
-        { duration: '15s', target: 80 }, // traffic ramp-up from 1 to 80 users over 30 seconds.
-        { duration: '90s', target: 80 }, // stay at 80 users for 1 minute
+        { duration: '15s', target: 80 }, // traffic ramp-up from 1 to 80 users over 15 seconds.
+        { duration: '90s', target: 80 }, // stay at 80 users for 90 seconds
         { duration: '15s', target: 0 }, // ramp-down to 0 users
     ],
 };
